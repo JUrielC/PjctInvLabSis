fcbbe6e3cd4347e0cb2144df706f420e54f36b1d
JUrielC <uri.jucv@gmail.com>
2024-05-26 16:53:32 -0600

    login regresa nombre de user, actualización de prestamos c y r

--- backend/routes/prestamo_routes.js
+++ backend/routes/prestamo_routes.js
@@ -4,7 +4,9 @@ const {check_token} = require('../middlewares/check_token.js')
 const {check_rol} = require('../middlewares/check_rol.js')
 
 const { body, param } = require("express-validator")
-const { post_prestamo, get_bajas, delete_baja} = require('../controllers/prestamos_controller.js');
+const { post_prestamo, get_prestamos_activos, delete_baja} = require('../controllers/prestamos_controller.js');
+
+router.get('/', get_prestamos_activos)
 
 router.post('/', post_prestamo)
 
