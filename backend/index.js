217cb2fa20543bada4e6a25c5ed4517a287c3a34
JUrielC <uri.jucv@gmail.com>
2024-06-13 20:35:32 -0600

    sin cors

--- backend/index.js
+++ backend/index.js
@@ -5,14 +5,14 @@ const cors = require('cors');
 app.use(cors()); 
 
 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
 });
-
+ */
 
 
 app.use(express.json());
