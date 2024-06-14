305aac5ef8a535285bfc2530f009b32de148ed48
JUrielC <uri.jucv@gmail.com>
2024-06-13 19:50:16 -0600

    cors 2.0

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,6 @@ const cors = require('cors');
 app.use(cors()); 
 
 
-app.listen(process.env.NODE_PORT, () => {
-    console.log('server running '+ process.env.NODE_PORT);
-});
-
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
@@ -16,6 +12,13 @@ app.use((req, res, next) => {
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
 })
+
+
+
+
+app.listen(process.env.NODE_PORT, () => {
+    console.log('server running '+ process.env.NODE_PORT);
+});
 
 app.use(express.json());
 app.use('/herramientas', require('./routes/herramientas_routes'))
