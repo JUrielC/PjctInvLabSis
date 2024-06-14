60661a5c7b00917e37ca23205b81b93949633761
JUrielC <uri.jucv@gmail.com>
2024-06-13 20:37:40 -0600

    cors con lh3000

--- backend/index.js
+++ backend/index.js
@@ -5,14 +5,14 @@ const cors = require('cors');
 app.use(cors()); 
 
 
-/* app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+/**/ app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000/');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
 });
- */
+ 
 
 
 app.use(express.json());
