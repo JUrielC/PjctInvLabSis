e4cc23165d628e650c4ab145815f3151d5af042a
JUrielC <uri.jucv@gmail.com>
2024-06-13 20:50:33 -0600

    asdfghjkjhgfdsa

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const app = express();
 require('dotenv').config({path: 'backend/.env'});
-//const cors = require('cors');
-import cors from 'cors';
+const cors = require('cors');
 
-app.use(cors()); 
+// Habilitar CORS
+app.use(cors());
 
-
-/**/ app.use((req, res, next) => {
+// Middleware para cabeceras CORS
+app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-   /*  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Methods');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE'); */
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
 });
- 
 
 
 app.use(express.json());
